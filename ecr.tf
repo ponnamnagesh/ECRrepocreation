e9a71a98f29eb6d06ac0f7cba99463f8aa098abf
ponnamnagesh <74391064+ponnamnagesh@users.noreply.github.com>
2022-06-16 09:20:44 -0500

    Create ecr.tf

---
+++ ecr.tf
@@ -0,0 +1,10 @@
+
+
+resource "aws_ecr_repository" "cvecr" {
+  name                 = "claimvisionecr"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
