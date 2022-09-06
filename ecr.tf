327322b786d006909db8fbc8f24af77ad31b3fc1
ponnamnagesh <74391064+ponnamnagesh@users.noreply.github.com>
2022-09-06 14:41:36 -0500

    Update ecr.tf

--- ecr.tf
+++ ecr.tf
@@ -1,6 +1,6 @@
 
 
-resource "aws_ecr_repository" "cvecr" {
+resource "aws_ecr_repository" "ecs-koffee-luv-home" {
   name                 = "ecs-koffee-luv-home"
   image_tag_mutability = "MUTABLE"
 
