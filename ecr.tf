1c03787b4989df96c8a8cac82c9814aa12125bf7
ponnamnagesh <74391064+ponnamnagesh@users.noreply.github.com>
2022-09-06 14:35:40 -0500

    Update ecr.tf

--- ecr.tf
+++ ecr.tf
@@ -1,7 +1,7 @@
 
 
 resource "aws_ecr_repository" "cvecr" {
-  name                 = "claimvisionecr"
+  name                 = "ecs-koffee-luv-home"
   image_tag_mutability = "MUTABLE"
 
   image_scanning_configuration {
